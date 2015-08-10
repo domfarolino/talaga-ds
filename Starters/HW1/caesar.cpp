4dc370b7a9bb8fc390737452b0e96e4ed967ea95
Paul Talaga <paul.talaga@uc.edu>
2015-08-09 21:11:14 -0400

    HW1 starters

---
+++ Starters/HW1/caesar.cpp
@@ -0,0 +1,23 @@
+/*
+	Caesar cipher
+	If -f is given, use text from file, otherwise STDIN.
+	-r <number> is REQUIRED and represents how much to rotate the alphabet.
+	//TODO
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "unistd.h"
+
+using namespace std;
+
+//TODO
+int main(int argc, char **argv){
+	//TODO
+	
+	return 0;
+}
