2f8fe6d50ddd7b5ff2175fdad222b2319bbbfc4b
Paul Talaga <paul.talaga@uc.edu>
2015-09-01 00:52:50 -0400

    Lab2/HW2 Starters

---
+++ Starters/Lab2HW2/Book.cpp
@@ -0,0 +1,68 @@
+/*
+	Implementation for Book class.
+	Paul Talaga
+	August 2015
+*/
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include "Book.h"
+
+using namespace std;
+
+Book::Book(string filename){
+   // TODO
+}
+  
+Book::Book(string title, string first, string last, string text){
+   // TODO
+}
+  
+// Getters
+string Book::getTitle(){
+   // TODO
+  return "";
+}
+  
+string Book::getFullAuthor(){
+   // TODO
+  return "";
+}
+  
+string Book::getAuthorFirst(){
+   // TODO
+  return "";
+}
+  
+string Book::getAuthorLast(){
+   // TODO
+  return "";
+}
+  
+unsigned int Book::getCharacterCount(){
+   // TODO
+  return 0;
+}
+  
+unsigned int Book::getLineCount(){
+   // TODO
+  return 0;
+}
+  
+unsigned int Book::getWordCount(string word){
+   // TODO
+  return 0;
+}
+
+vector<float> Book::letterPercent(){
+   // TODO
+  vector<float> ret;
+  return ret;
+}
+  
+string Book::getText(){
+   // TODO
+  return "";
+}
---
+++ Starters/Lab2HW2/Library.cpp
@@ -0,0 +1,60 @@
+/*
+	Library implementation.
+	Paul Talaga
+	August 2015
+*/
+
+#include <string>
+#include <string.h>
+#include <dirent.h>
+#include <iostream>
+#include <cmath>
+
+#include "Library.h"
+
+
+using namespace std;
+
+Library::Library(string folder){
+  // Open a directory and read the contents
+  DIR* directory = opendir(folder.c_str());
+  dirent* ent = readdir(directory);
+  while(ent){
+    if(strlen(ent->d_name) > 2){  // strlen gets the length of a c-string (char*)
+      // Assume if the length > 2 it is a valid file
+      // TODO: chage inside here to create and store new books!
+      cout << ent->d_name << endl;
+    }
+    ent = readdir(directory);
+  }
+}
+
+Library::~Library(){
+ // TODO:
+}
+
+int Library::getBookCount(){
+  // TODO:
+  return books.size();
+}
+
+vector<string> Library::getBookTitles(){
+  // TODO:
+  vector<string> ret;
+ return ret;
+}
+  
+Book* Library::getBook(unsigned int i){
+  // TODO:
+  return (Book*)NULL;
+}
+
+Book* Library::getBook(string title){
+// TODO:
+  return (Book*)NULL;
+}
+
+float Library::getSimilarity(Book* one, Book* two){
+  // TODO:
+  return 0.0;
+}
