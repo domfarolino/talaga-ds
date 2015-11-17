6cc6f51bad546e66b471ac1209974a4e9bcd0a4c
Paul Talaga <paul.talaga@uc.edu>
2015-11-16 20:51:38 -0500

    Lab12 Starter

---
+++ Starters/Lab12/MaxHeap.h
@@ -0,0 +1,45 @@
+#ifndef MAXHEAP_H
+#define MAXHEAP_H 
+/*    file MaxHeap.h    
+      author <-- Fill in your name here -->
+      date <-- Fill this in -->
+
+      @description Implements a templated maximum queue, using a
+      heap structure.
+*/
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+template <class T> 
+class MaxHeap {
+public: 
+
+MaxHeap(){ 
+  // TODO: Fill me in
+}
+
+void push(T value){
+  // TODO: Fill me in
+}
+
+void pop(){
+  // TODO: Fill me in
+}
+
+T top(){
+  // TODO: Fill me in
+}
+
+unsigned int size(){
+  // TODO: Fill me in
+}
+
+private:
+// TODO: Fill me in
+
+};
+
+#endif
---
+++ Starters/Lab12/MaxHeap_test.h
@@ -0,0 +1,23 @@
+#ifndef MAXHEAP_TEST_H
+#define MAXHEAP_TEST_H
+/*
+  MinHeap_test.h
+  Unit tests for MinHeap
+  
+*/
+
+#include "MaxHeap.h"
+#include <stdexcept>
+#include <string>
+#include <queue>
+#include <stdlib.h>
+
+#include <cxxtest/TestSuite.h>
+
+class MaxHeap : public CxxTest::TestSuite {
+public:
+// TODO: Fill me in
+
+};
+
+#endif
